d58550191cbd945f783759c064fff1784a2f1425
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-04-23 12:42:15 -0300

    Configured global styled component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return <h1>GoBarber Web</h1>;
-}
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>GoBarber Web</h1>
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
