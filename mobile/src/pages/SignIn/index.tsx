702d5de7613776ba34c1fd2f4e6b2af2c7c0c556
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-05-02 22:01:15 -0300

    Added logo assets and configured types

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import { Cantainer } from './styles';
 
+import logoImg from '../../assets/logo.png';
+
 const SignIn: React.FC = () => {
-  return <Cantainer />;
+  return (
+    <Cantainer>
+      <Image source={logoImg} />
+    </Cantainer>
+  );
 };
 
 export default SignIn;
