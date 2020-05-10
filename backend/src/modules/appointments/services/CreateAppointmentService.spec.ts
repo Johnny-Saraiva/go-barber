1e734fc5a131e9a3e58b0558517828144facf8bb
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-05-10 11:57:25 -0300

    Configured jest file and FakeAppointment test

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,20 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakesAppointmentsRepository from '@modules/appointments/repositories/fakes/FakesAppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('shoud be able to create a new appointment', async () => {
+    const fakesAppointmentsRepository = new FakesAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakesAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
+  });
 });
