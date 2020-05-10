5168b02dc1515ddceae9caeea2322aeaf9a44359
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-05-10 13:28:34 -0300

    Finished Appointment test

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,10 +1,10 @@
+import AppError from '@shared/errors/AppError';
 import FakesAppointmentsRepository from '@modules/appointments/repositories/fakes/FakesAppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 describe('CreateAppointment', () => {
   it('shoud be able to create a new appointment', async () => {
     const fakesAppointmentsRepository = new FakesAppointmentsRepository();
-
     const createAppointment = new CreateAppointmentService(
       fakesAppointmentsRepository,
     );
@@ -16,5 +16,26 @@ describe('CreateAppointment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123123123');
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakesAppointmentsRepository = new FakesAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakesAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
