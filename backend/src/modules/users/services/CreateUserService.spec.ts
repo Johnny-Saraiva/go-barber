32bcd70aa43ddb5516d5b5adfbc3044f67e4b165
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-05-11 08:05:21 -0300

    Configured AuthenticateUserService test

--- backend/src/modules/users/services/CreateUserService.spec.ts
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -2,11 +2,16 @@ import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -19,7 +24,11 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with same email', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     await createUser.execute({
       name: 'John Doe',
