f7323d2938a75dc929c49d0fd1dca8dcbe784605
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-05-14 19:44:06 -0300

    Added EtherealMail for test reset password for email

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('files', express.static(uploadConfig.directory));
+app.use('files', express.static(uploadConfig.uploadFolders));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
