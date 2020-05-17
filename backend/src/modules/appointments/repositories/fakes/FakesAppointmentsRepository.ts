33acdc4279900e250c1a042f36b98a2b8b35a88e
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-05-17 17:22:35 -0300

    Configured create appointment

--- backend/src/modules/appointments/repositories/fakes/FakesAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/fakes/FakesAppointmentsRepository.ts
@@ -55,11 +55,12 @@ class AppointmentsRepository implements IAppointmentsRepository {
 
   public async create({
     provider_id,
+    user_id,
     date,
   }: ICreateAppointmentDTO): Promise<Appointments> {
     const appointment = new Appointments();
 
-    Object.assign(appointment, { id: uuid(), date, provider_id });
+    Object.assign(appointment, { id: uuid(), date, provider_id, user_id });
 
     this.appointments.push(appointment);
 
--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -14,15 +14,17 @@ describe('ListProviderDayAvailability', () => {
   it('should be able to list the day availability from profile', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      user_id: 'user',
       date: new Date(2020, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      user_id: 'user',
       date: new Date(2020, 4, 20, 15, 0, 0),
     });
 
-    jest.spyOn(Date, 'now').mockImplementation(() => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
       return new Date(2020, 4, 20, 11).getTime();
     });
 
