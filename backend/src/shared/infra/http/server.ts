3b17a18f24dc2edcdc4f2001842d7570b04a9d5c
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-05-21 13:28:20 -0300

    Added classTransform to edit view of User entity

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('files', express.static(uploadConfig.uploadFolders));
+app.use('/files', express.static(uploadConfig.uploadFolders));
 app.use(routes);
 
 app.use(errors());
