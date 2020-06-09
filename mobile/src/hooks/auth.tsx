f10fc60db011e45ddd92ce5373ebacb720d94dc8
Johnny-Saraiva <johnny.saraiva.ads@gmail.com>
2020-06-09 11:55:55 -0300

    configured authentication

---
+++ mobile/src/hooks/auth.tsx
@@ -0,0 +1,106 @@
+import React, {
+  createContext,
+  useCallback,
+  useState,
+  useContext,
+  useEffect,
+} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../services/api';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: User;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+  updateUser(user: User): void;
+}
+
+const Auth = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>({} as AuthState);
+
+  useEffect(() => {
+    async function laodStoragedData(): Promise<void> {
+      const [token, user] = await AsyncStorage.multiGet([
+        '@Gobarber:token',
+        '@Gobarber:user',
+      ]);
+
+      if (token[1] && user[1]) {
+        setData({ token: token[1], user: JSON.parse(user[1]) });
+      }
+    }
+    laodStoragedData();
+  }, []);
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+    const { token, user } = response.data;
+
+    await AsyncStorage.multiSet([
+      ['@Gobarber:token', token],
+      ['@Gobarber:user', JSON.stringify(user)],
+    ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(['@Gobarber:token', '@Gobarber:user']);
+
+    setData({} as AuthState);
+  }, []);
+
+  const updateUser = useCallback(
+    async (user: User) => {
+      await AsyncStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
+
+  return (
+    <Auth.Provider value={{ user: data.user, signIn, signOut, updateUser }}>
+      {children}
+    </Auth.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(Auth);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
